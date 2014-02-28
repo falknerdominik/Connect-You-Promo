6d6e3e7634362e14112fe43e867dc8150f1afb13
Lucas Dikany <dikany_u1@bbs-rohrbach.at>
2014-02-28 21:49:38 +0100

    / ‘js/promo.js’

--- js/promo.js
+++ js/promo.js
@@ -39,3 +39,16 @@ function resizePanel() {
     $('#wrapper').scrollTo($('a.selected').attr('href'), 0);
 
 }
+
+function setHeight() {
+	var img = document.getElementById("login_bg");
+	var height = img.height / 2;
+	var marginTop = height / 2 * -1;
+	var wrapperWidth = document.getElementById("login_bg_wrapper");
+	var marginLeft = wrapperWidth.width / 4;
+	document.getElementById("login_bg_wrapper").style.height = height;
+	document.getElementById("login_bg").style.marginTop = marginTop;
+	document.getElementById("login_bg").style.marginLeft = marginLeft;
+}
+window.onload = setHeight;
+window.onresize = setHeight;
